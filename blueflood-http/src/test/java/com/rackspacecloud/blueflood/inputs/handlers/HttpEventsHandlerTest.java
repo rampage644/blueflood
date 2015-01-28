011d196bb554867e4270213fb132989ae7f94190
Sergei Turukin <rampage644@gmail.com>
2015-01-28 16:26:44 +0200

    Add tests to implement

--- blueflood-http/src/test/java/com/rackspacecloud/blueflood/inputs/handlers/HttpEventsHandlerTest.java
+++ blueflood-http/src/test/java/com/rackspacecloud/blueflood/inputs/handlers/HttpEventsHandlerTest.java
@@ -5,8 +5,6 @@ import com.rackspacecloud.blueflood.http.HTTPRequestWithDecodedQueryParams;
 import com.rackspacecloud.blueflood.io.GenericElasticSearchIO;
 import com.rackspacecloud.blueflood.outputs.handlers.HttpEventsHandler;
 import junit.framework.Assert;
-import org.apache.zookeeper.proto.CreateRequest;
-import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.channel.*;
@@ -14,7 +12,6 @@ import org.jboss.netty.handler.codec.http.*;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -52,17 +49,18 @@ public class HttpEventsHandlerTest {
         events.add(event);
 
         final String requestBody = new ObjectMapper().writeValueAsString(events.get(0));
-        return createRequest(HttpMethod.POST, requestBody);
+        return createRequest(HttpMethod.POST, "", requestBody);
     }
 
-    private HttpRequest createGetRequest(String requestQuery) {
-        return createRequest(HttpMethod.GET, requestQuery);
+    private HttpRequest createGetRequest(String uri) {
+        return createRequest(HttpMethod.GET, uri, "");
     }
 
-    private HttpRequest createRequest(HttpMethod method, String requestBody) {
-        DefaultHttpRequest rawRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, "/v2.0/" + TENANT + "/events");
+    private HttpRequest createRequest(HttpMethod method, String uri, String requestBody) {
+        DefaultHttpRequest rawRequest = new DefaultHttpRequest(HttpVersion.HTTP_1_1, method, "/v2.0/" + TENANT + "/events/" + uri);
         rawRequest.setHeader("tenantId", TENANT);
-        rawRequest.setContent(ChannelBuffers.copiedBuffer(requestBody.getBytes()));
+        if (!requestBody.equals(""))
+            rawRequest.setContent(ChannelBuffers.copiedBuffer(requestBody.getBytes()));
         return HTTPRequestWithDecodedQueryParams.createHttpRequestWithDecodedQueryParams(rawRequest);
     }
 
@@ -82,13 +80,32 @@ public class HttpEventsHandlerTest {
 
     @Test
     public void testElasticSearchSearchCalledWhenGet() {
-        handler.handle(context, createGetRequest(""));
+        testQuery("", new HashMap<String, List<String>>());
+    }
+
+    private void testQuery(String query, Map<String, List<String>> params) {
+        handler.handle(context, createGetRequest(query));
         try {
-            Map<String, List<String>> params = new HashMap<String, List<String>>();
             verify(searchIO).search(TENANT, params);
         }
         catch (Exception e) {
             Assert.fail(e.getMessage());
         }
+    }
+
+    @Test public void testMalformedEventPut() {
+        Assert.fail("Not implemented");
+    }
+
+    @Test public void testQueryParametersParse() {
+        Assert.fail("Not implemented");
+    }
+
+    @Test public void testMinimumEventPut() {
+        Assert.fail("Not implemented");
+    }
+
+    @Test public void testApplyingCurrentTimeWhenEmpty() {
+        Assert.fail("Not implemented");
     }
 }
