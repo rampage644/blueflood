22d63c7a82be73d78e11cf1c4e2f1cfa2b59a6d4
Sergei Turukin <rampage644@gmail.com>
2015-02-01 20:08:11 +0200

    WIP parse date/time

--- blueflood-http/src/main/java/com/rackspacecloud/blueflood/utils/DateTimeParser.java
+++ blueflood-http/src/main/java/com/rackspacecloud/blueflood/utils/DateTimeParser.java
@@ -1,10 +1,105 @@
 package com.rackspacecloud.blueflood.utils;
 
-
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class DateTimeParser {
     public static DateTime parse(String s) {
-        return new DateTime();
+        String stringToParse = s.replace(" ", "").replace(",", "").replace("_", "");
+        if (StringUtils.isNumeric(stringToParse)) {
+            return new DateTime(Long.parseLong(stringToParse));
+        }
+        try {
+            DateTimeFormatter formatter = DateTimeFormat.forPattern("HH:mmyyyyMMdd");
+            return formatter.parseDateTime(stringToParse);
+        }
+        catch (IllegalArgumentException e) {
+
+        }
+
+        String timezone = "";
+        if (stringToParse.contains("+")) {
+            String[] timezoneSplit = stringToParse.split("\\+", 1);
+            stringToParse = timezoneSplit[0];
+            timezone = timezoneSplit.length > 1 ? timezoneSplit[1] : "";
+        } else if (stringToParse.contains("-")) {
+            String[] timezoneSplit = stringToParse.split("-", 1);
+            stringToParse = timezoneSplit[0];
+            timezone = timezoneSplit.length > 1 ? timezoneSplit[1] : "";
+        }
+
+        return parseTime(stringToParse);
+    }
+
+    private static DateTime parseTime(String stringToParse) {
+        DateTime resultDateTime = new DateTime().withTime(0, 0, 0, 0);
+
+        if (stringToParse.equals("now"))
+            return resultDateTime;
+
+        if (stringToParse.contains(":")) {
+            int hour = 0;
+            int minute = 0;
+            int semicolonPosition = stringToParse.indexOf(":");
+            if (semicolonPosition != -1) {
+                hour = Integer.parseInt(stringToParse.substring(0, semicolonPosition));
+                minute = Integer.parseInt(stringToParse.substring(semicolonPosition + 1, semicolonPosition + 3));
+                stringToParse = stringToParse.substring(semicolonPosition + 3);
+
+                try {
+                    if (stringToParse.substring(0, 2).equals("am")) {
+                        stringToParse = stringToParse.substring(2);
+                    } else if (stringToParse.substring(0, 2).equals("pm")) {
+                        hour = (hour + 12) % 24;
+                        stringToParse = stringToParse.substring(2);
+                    }
+                }
+                catch (StringIndexOutOfBoundsException e) {}
+            }
+            resultDateTime = resultDateTime.withHourOfDay(hour).withMinuteOfHour(minute);
+        }
+
+        if (stringToParse.startsWith("noon")) {
+            resultDateTime = resultDateTime.withHourOfDay(12).withMinuteOfHour(0);
+            stringToParse = stringToParse.substring(0, "noon".length());
+        } else if (stringToParse.startsWith("teatime")) {
+            resultDateTime = resultDateTime.withHourOfDay(16).withMinuteOfHour(0);
+            stringToParse = stringToParse.substring(0, "teatime".length());
+        } else if (stringToParse.startsWith("midnight")) {
+            resultDateTime = resultDateTime.withHourOfDay(0).withMinuteOfHour(0);
+            stringToParse = stringToParse.substring(0, "midnight".length());
+        }
+
+        if (stringToParse.equals("today")) {
+            // do nothing
+        } else if (stringToParse.equals("tomorrow")) {
+            resultDateTime = resultDateTime.plusDays(1);
+        } else if (stringToParse.equals("yesterday")) {
+            resultDateTime = resultDateTime.minusDays(1);
+        } else {
+            // do nothing
+        }
+
+        String[] datePatterns = {"MM/dd/YY", "MM/dd/YYYY", "YYYYMMdd", "MMMMdd", "EEE"};
+        for (String s : datePatterns) {
+            try {
+                DateTime date = parseDate(stringToParse, s);
+                resultDateTime = resultDateTime.withDate(date.year().get(), date.monthOfYear().get(), date.dayOfMonth().get());
+                break;
+            }
+            catch (IllegalArgumentException e) {
+                continue;
+            }
+        }
+
+
+        return resultDateTime;
+    }
+
+    private static DateTime parseDate(String stringToParse, String format) {
+        return DateTimeFormat.forPattern(format).parseDateTime(stringToParse);
     }
 }
--- blueflood-http/src/test/java/com/rackspacecloud/blueflood/utils/DateTimeParserTest.java
+++ blueflood-http/src/test/java/com/rackspacecloud/blueflood/utils/DateTimeParserTest.java
@@ -2,11 +2,119 @@ package com.rackspacecloud.blueflood.utils;
 
 import com.rackspacecloud.blueflood.utils.DateTimeParser;
 import junit.framework.Assert;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
 
 public class DateTimeParserTest {
     @Test
-    public void test() {
-        Assert.fail("Not implemented");
+    public void testFromUnixTimestamp() {
+        long unixTimestamp = 1422792604;
+
+        Assert.assertEquals(DateTimeParser.parse(Long.toString(unixTimestamp)),
+                new DateTime(unixTimestamp));
     }
+
+    @Test
+    public void testPlainTimeDateFormat() {
+        // %H:%M%Y%m%d
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("HH:mmyyyyMMdd");
+        String dateTimeWithSpace = "10:55 2014 12 20";
+        String dateTimeWithUnderscore = "10:55_2014_12_20";
+
+        Assert.assertEquals(DateTimeParser.parse(dateTimeWithSpace),
+                new DateTime(formatter.parseDateTime(dateTimeWithSpace.replace(" ", ""))));
+
+        Assert.assertEquals(DateTimeParser.parse(dateTimeWithUnderscore),
+                new DateTime(formatter.parseDateTime(dateTimeWithUnderscore.replace("_", ""))));
+    }
+
+    @Test
+    public void testNowKeyword() {
+        String nowTimestamp = "now";
+
+        Assert.assertEquals(DateTimeParser.parse(nowTimestamp),
+                new DateTime());
+    }
+
+    @Test
+    public void testTimezone() {
+        String timestampWithTimezone = "now + 06:00";
+        String timestampWithZeroTimeZone = "now + 00:00";
+
+        Assert.assertEquals(DateTimeParser.parse(timestampWithTimezone),
+                DateTimeParser.parse(timestampWithZeroTimeZone).plusHours(6));
+    }
+
+    @Test
+    public void testRegularHourMinute() {
+        String hourMinuteTimestamp = "12:24";
+        String hourMinuteWithAm = "9:13am";
+        String hourMinuteWithPm = "09:13pm";
+
+        Assert.assertEquals(DateTimeParser.parse(hourMinuteTimestamp),
+                referenceDateTime().withHourOfDay(12).withMinuteOfHour(24));
+
+        Assert.assertEquals(DateTimeParser.parse(hourMinuteWithAm),
+                referenceDateTime().withHourOfDay(9).withMinuteOfHour(13));
+
+        Assert.assertEquals(DateTimeParser.parse(hourMinuteWithPm),
+                referenceDateTime().withHourOfDay(21).withMinuteOfHour(13));
+    }
+
+    @Test
+    public void testHourMinuteKeywords() {
+        String noonTimestamp = "noon";
+        String teatimeTimestamp = "teatime";
+        String midnightTimestamp = "midnight";
+
+        Assert.assertEquals(DateTimeParser.parse(noonTimestamp),
+                referenceDateTime().withHourOfDay(12).withMinuteOfHour(0));
+
+        Assert.assertEquals(DateTimeParser.parse(teatimeTimestamp),
+                referenceDateTime().withHourOfDay(16).withMinuteOfHour(0));
+
+        Assert.assertEquals(DateTimeParser.parse(midnightTimestamp),
+                referenceDateTime().withHourOfDay(0).withMinuteOfHour(0));
+    }
+
+    @Test
+    public void testDayKeywords() {
+        String todayTimestamp = "today";
+        String yesterdayTimestamp = "yesterday";
+        String tomorrowTimeStamp = "tomorrow";
+
+        Assert.assertEquals(DateTimeParser.parse(todayTimestamp),
+                referenceDateTime());
+
+        Assert.assertEquals(DateTimeParser.parse(yesterdayTimestamp),
+                referenceDateTime().minusDays(1));
+
+        Assert.assertEquals(DateTimeParser.parse(tomorrowTimeStamp),
+                referenceDateTime().plusDays(1));
+    }
+
+    @Test
+    public void testDateFormats() {
+        testFormat("12/30/14", new DateTime(2014, 12, 30, 0, 0, 0, 0));
+        testFormat("12/30/2014", new DateTime(2014, 12, 30, 0, 0, 0, 0));
+        testFormat("Jul 30", new DateTime(2014, 07, 30, 0, 0, 0, 0));
+        testFormat("20141230", new DateTime(2014, 12, 30, 0, 0, 0, 0));
+    }
+
+    @Test
+    public void testDayOfWeekFormat() {
+        DateTime date = DateTimeParser.parse("Sun");
+        Assert.assertEquals(date.getDayOfWeek(), 7);
+    }
+
+    private void testFormat(String dateString, DateTime date) {
+        Assert.assertEquals(DateTimeParser.parse(dateString), date);
+    }
+
+    private static DateTime referenceDateTime() {
+        return new DateTime().withSecondOfMinute(0).withMillisOfSecond(0);
+    }
+
 }
