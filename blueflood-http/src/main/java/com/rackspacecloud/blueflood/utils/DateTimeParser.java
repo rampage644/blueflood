d028353fc54b176de39ce4bc8bec4b80c748c18a
Sergei Turukin <rampage644@gmail.com>
2015-02-01 13:45:15 +0200

    Future DateTimeParser class and its tests

---
+++ blueflood-http/src/main/java/com/rackspacecloud/blueflood/utils/DateTimeParser.java
@@ -0,0 +1,10 @@
+package com.rackspacecloud.blueflood.utils;
+
+
+import org.joda.time.DateTime;
+
+public class DateTimeParser {
+    public static DateTime parse(String s) {
+        return new DateTime();
+    }
+}
---
+++ blueflood-http/src/test/java/com/rackspacecloud/blueflood/utils/DateTimeParserTest.java
@@ -0,0 +1,12 @@
+package com.rackspacecloud.blueflood.utils;
+
+import com.rackspacecloud.blueflood.utils.DateTimeParser;
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class DateTimeParserTest {
+    @Test
+    public void test() {
+        Assert.fail("Not implemented");
+    }
+}
